36d8ef4bee90d710a9c202282ef8cb64b65f245d
pranoy12001 <pranoy.mbstu12001@gmail.com>
2021-07-05 00:27:44 +0600

    added coupon service

---
+++ src/main/java/com/pranoy/spring/security/repos/CouponRepository.java
@@ -0,0 +1,8 @@
+package com.pranoy.spring.security.repos;
+
+import com.pranoy.spring.security.model.Coupon;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CouponRepository extends JpaRepository<Coupon, Long> {
+    Coupon findByCode(String code);
+}
