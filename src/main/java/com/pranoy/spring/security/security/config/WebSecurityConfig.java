c1662b6291d468668f091bc742466ed343915915
pranoy12001 <pranoy.mbstu12001@gmail.com>
2021-07-13 01:37:27 +0600

    updated mvc matchers

--- src/main/java/com/pranoy/spring/security/security/config/WebSecurityConfig.java
+++ src/main/java/com/pranoy/spring/security/security/config/WebSecurityConfig.java
@@ -24,9 +24,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.httpBasic();
+        http.formLogin();
         http.authorizeRequests()
-                .mvcMatchers(HttpMethod.GET, "/couponapi/coupons/**").hasAnyRole("USER", "ADMIN")
+                .mvcMatchers(HttpMethod.GET, "/couponapi/coupons/{code:^[A-Z]*$}", "/", "index", "/showCreateCoupon", "createCoupon").hasAnyRole("USER", "ADMIN")
                 .mvcMatchers(HttpMethod.POST, "/couponapi/coupons").hasRole("ADMIN")
                 .anyRequest().denyAll()
                 .and()
