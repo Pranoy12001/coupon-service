bba25fbe0c7e34af2a844bbb2b8ed669959737f2
pranoy12001 <pranoy.mbstu12001@gmail.com>
2021-07-14 01:58:06 +0600

    added some controller

--- src/main/java/com/pranoy/spring/security/controllers/CouponController.java
+++ src/main/java/com/pranoy/spring/security/controllers/CouponController.java
@@ -1,10 +1,19 @@
 package com.pranoy.spring.security.controllers;
 
+import com.pranoy.spring.security.model.Coupon;
+import com.pranoy.spring.security.repos.CouponRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class CouponController {
+    @Autowired
+    private CouponRepository couponRepository;
 
     @GetMapping("/")
     public String index() {
@@ -14,5 +23,23 @@ public class CouponController {
     @GetMapping("/showCreateCoupon")
     public String showCreateCoupon() {
         return "createCoupon";
+    }
+
+    @PostMapping("/saveCoupon")
+    public String save(Coupon coupon, Model model) {
+        Coupon newCoupon = couponRepository.save(coupon);
+        return "createResponse";
+    }
+
+    @GetMapping("/showGetCoupon")
+    public String showGetCoupon() {
+        return "getCoupon";
+    }
+
+    @PostMapping("/getCoupon")
+    public ModelAndView getCoupon(String code) {
+        ModelAndView modelAndView = new ModelAndView("couponDetails");
+        modelAndView.addObject(couponRepository.findByCode(code));
+        return modelAndView;
     }
 }
