2fa71c8528e966bc90d6452f15579bbfa82ee669
pranoy12001 <pranoy.mbstu12001@gmail.com>
2021-07-22 01:43:55 +0600

    implement custom login

---
+++ src/main/java/com/pranoy/spring/security/controllers/UserController.java
@@ -0,0 +1,29 @@
+package com.pranoy.spring.security.controllers;
+
+import com.pranoy.spring.security.security.service.SecurityService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class UserController {
+    @Autowired
+    SecurityService securityService;
+
+    @GetMapping("/")
+    public String showLoginPage() {
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public String login(String email, String password) {
+        boolean loginResponse = securityService.login(email, password);
+
+        if (loginResponse) {
+            return "index";
+        }
+
+        return "login";
+    }
+}
